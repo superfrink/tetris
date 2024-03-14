7373191910c7106f09309c9e1910d639baca8f5c
Chad Clark <chad.clark@gmail.com>
2024-03-14 02:19:57 +0000

    Move Display to display.go.

--- src/tetris_console_game.go
+++ src/tetris_console_game.go
@@ -5,57 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
 
 	"github.com/nsf/termbox-go"
 	"superfrink.net/tetris/engine"
 	"superfrink.net/tetris/streamer"
 )
-
-// DOC: Display wraps a mutex around termbox writes.
-type Display struct {
-	mu sync.Mutex
-}
-
-func (d *Display) Clear(a termbox.Attribute, b termbox.Attribute) {
-	d.mu.Lock()
-	termbox.Clear(a, b)
-	d.mu.Unlock()
-}
-
-func (d *Display) Close() {
-	d.mu.Lock()
-	termbox.Close()
-	d.mu.Unlock()
-}
-
-func (d *Display) Flush() {
-	d.mu.Lock()
-	termbox.Flush()
-	d.mu.Unlock()
-}
-
-func (d *Display) Init() error {
-	d.mu.Lock()
-	val := termbox.Init()
-	d.mu.Unlock()
-	return val
-}
-
-func (d *Display) PollEvent() termbox.Event {
-	val := termbox.PollEvent()
-	return val
-}
-
-func (d *Display) TBPrint(y int, x int, str string) {
-	d.mu.Lock()
-	// tbprint is based on from https://github.com/jjinux/gotetris/
-	for _, c := range str {
-		termbox.SetCell(x, y, c, termbox.ColorWhite, termbox.ColorBlack)
-		x++
-	}
-	d.mu.Unlock()
-}
 
 var display Display
 
