57ae7b845b18dc61a9db18736e78028d4bdfa944
Chad Clark <chad.clark@gmail.com>
2026-01-24 15:42:19 +0000

    work in progress from 2024.

--- cmd/game_recorder/game_recorder.go
+++ cmd/game_recorder/game_recorder.go
@@ -7,29 +7,52 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/klauspost/compress/gzip"
 	"superfrink.net/tetris/streamer"
 )
 
-type GameRecorder struct {
-	mu       sync.Mutex
-	messages []streamer.Message
+type MoveRecord struct {
+	Field [][]int
+	Move  uint8
+	Piece uint8
+	PCol  uint8
+	PRot  uint8
+	PRow  uint8
 }
 
-func (r *GameRecorder) QueueMove(message streamer.Message) {
+func MessageToMoveRecord(message streamer.Message) MoveRecord {
+	move := MoveRecord{
+		Field: message.Game.Field,
+		Move:  message.Move,
+		Piece: uint8(message.Game.Piece),
+		PCol:  uint8(message.Game.PiecePosCol),
+		PRot:  uint8(message.Game.PieceRotation),
+		PRow:  uint8(message.Game.PiecePosRow),
+	}
+	return move
+}
+
+type GameRecorder struct {
+	mu    sync.Mutex
+	moves []MoveRecord
+}
+
+func (r *GameRecorder) QueueMove(rec MoveRecord) {
 	r.mu.Lock()
-	r.messages = append(r.messages, message)
+	r.moves = append(r.moves, rec)
 	r.mu.Unlock()
 }
 
-func (r *GameRecorder) RecordMoveFile() {
+func (r *GameRecorder) RecordToFile() {
 	r.mu.Lock()
 
-	count := len(r.messages)
-	slog.Info("messages", "count", count)
+	count := len(r.moves)
+	slog.Info("messages to write to file", "count", count)
 
 	if count > 1 {
 
@@ -51,13 +74,15 @@ func (r *GameRecorder) RecordMoveFile() {
 		// DOC: gob doesn't like the Game.PRNG // encoder := gob.NewEncoder(fileGzip)
 		encoder := json.NewEncoder(fileGzip)
 
-		for _, message := range r.messages {
-			slog.Info("message", "move", message.Move)
+		for _, message := range r.moves {
+			slog.Debug("message", "move", message.Move)
 			err := encoder.Encode(message)
 			if err != nil {
 				slog.Error("encoding message", "err", fmt.Sprintf("%s", err))
 			}
 		}
+
+		r.moves = r.moves[:0]
 	}
 
 endOfFunction:
@@ -80,19 +105,19 @@ func dumpFile(filename string) error {
 	}
 	defer fileGzip.Close()
 
-	// third decode the messages
+	// third decode the record
 	decoder := json.NewDecoder(fileGzip)
 
 	for {
-		var message streamer.Message
-		err := decoder.Decode(&message)
+		var rec MoveRecord
+		err := decoder.Decode(&rec)
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			return fmt.Errorf("decoding message from file %q %w", filename, err)
+			return fmt.Errorf("decoding move record from file %q %w", filename, err)
 		}
-		slog.Info("decoded", "message", message)
+		slog.Info("decoded", "rec", rec)
 	}
 
 	return nil
@@ -130,27 +155,53 @@ func main() {
 	// GOAL: setup the game recorder
 
 	var recorder = GameRecorder{
-		messages: make([]streamer.Message, 0),
+		moves: make([]MoveRecord, 0),
 	}
 
-	// GOAL: connect to NATS and record the game moves
+	// GOAL: connect to NATS
 
 	stream := streamer.Streamer{}
 	stream.Connect(*natsUrl, *natsCredFile, "fixme")
 	streamMesgChan := stream.RecvChan()
 	slog.Info("Connected to NATS", "url", *natsUrl)
 
-	ticker := time.NewTicker(10 * time.Second)
-	for {
-		select {
-		case message := <-streamMesgChan:
-			slog.Debug("message", "message", fmt.Sprintf("%+v", message))
-			if message.Type == streamer.Move {
-				recorder.QueueMove(message)
-			}
-		case <-ticker.C:
+	// GOAL: write the game moves to file periodically
+
+	go func(rec *GameRecorder) {
+		ticker := time.NewTicker(600 * time.Second)
+
+		for {
+			<-ticker.C
 			slog.Info("tick")
-			recorder.RecordMoveFile()
+
+			recorder.RecordToFile()
+		}
+	}(&recorder)
+
+	// GOAL: write the game moves to file on signals USR1, HUP, INT, TERM
+
+	go func(rec *GameRecorder) {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1)
+		for {
+			sig := <-sigChan
+			slog.Info("signal arrived", "sig", sig.String())
+			rec.RecordToFile()
+			if sig == syscall.SIGTERM || sig == syscall.SIGINT {
+				os.Exit(0)
+			}
+		}
+	}(&recorder)
+
+	// GOAL: receive and record messages
+
+	for {
+		message := <-streamMesgChan
+		slog.Debug("message", "message", fmt.Sprintf("%+v", message))
+
+		if message.Type == streamer.Move {
+			record := MessageToMoveRecord(message)
+			recorder.QueueMove(record)
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,20 +3,27 @@ module superfrink.net/tetris
 go 1.22.1
 
 require (
+	github.com/klauspost/compress v1.17.2
 	github.com/nsf/termbox-go v1.1.1
+	github.com/pa-m/sklearn v0.0.0-20200711083454-beb861ee48b1
 	superfrink.net/tetris/engine v0.0.0-00010101000000-000000000000
 	superfrink.net/tetris/streamer v0.0.0-00010101000000-000000000000
 )
 
 require (
-	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/chewxy/math32 v1.0.4 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/nats-io/nats.go v1.33.1 // indirect
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pa-m/optimize v0.0.0-20190612075243-15ee852a6d9a // indirect
+	github.com/pa-m/randomkit v0.0.0-20191001073902-db4fd80633df // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/exp v0.0.0-20191129062945-2f5052295587 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/tools v0.0.0-20200225230052-807dcd883420 // indirect
+	gonum.org/v1/gonum v0.6.1 // indirect
 )
 
 replace superfrink.net/tetris/engine => ./engine
